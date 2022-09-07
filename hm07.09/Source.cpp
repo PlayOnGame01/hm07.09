86aab73d98fc91b76d745bec84b39a3f0d4e15ea
Artem <artemzhdanov1299@gmail.com>
2022-09-07 16:01:11 +0300

    Добавил точки 'y' и 'z'

--- hm07.09/Source.cpp
+++ hm07.09/Source.cpp
@@ -6,8 +6,8 @@ class Cord
 {
 private:
     char* cordinatx;
-    //char* cordy;
-    //char* cordez;
+    char* cordy;
+    char* cordez;
 
 public:
 
@@ -15,8 +15,8 @@ public:
     {
         cout << "Constructor by Cord\n";
         char* cordinatx = nullptr;
-        //char* cordy = nullptr;
-        //char* cordz = nullptr;
+        char* cordy = nullptr;
+        char* cordz = nullptr;
     }
 
     Cord(const char* x, const char* y, const char* z)
@@ -25,13 +25,13 @@ public:
         cordinatx = new char[strlen(x) + 1];
         strcpy_s(cordinatx, strlen(x) + 1, x);
 
-        ////cout << "Cord: y =\n";
-        //cordy = new char[strlen(y) + 1];
-        //strcpy_s(cordy, strlen(y) + 1, y);
+        //cout << "Cord: y =\n";
+        cordy = new char[strlen(y) + 1];
+        strcpy_s(cordy, strlen(y) + 1, y);
 
-        ////cout << "Cord: z =\n";
-        //cordez = new char[strlen(z) + 1];
-        //strcpy_s(cordez, strlen(z) + 1, z);
+        //cout << "Cord: z =\n";
+        cordez = new char[strlen(z) + 1];
+        strcpy_s(cordez, strlen(z) + 1, z);
 
     }
 
@@ -39,8 +39,8 @@ public:
     void Print()
     {
         cout << "Cord: x = " << cordinatx << "\n";
-        //cout << "Cord: y = " << cordy << "\n";
-        //cout << "Cord: z = " << cordez << "\n";
+        cout << "Cord: y = " << cordy << "\n";
+        cout << "Cord: z = " << cordez << "\n";
     }
     void Input() {
         char buf[200];
@@ -50,20 +50,20 @@ public:
             cout << cordinatx << " is removed! \n";
             delete[]cordinatx;
         }
-        //cordy = new char[strlen(buf) + 1];
-        //strcpy_s(cordy, strlen(buf) + 1, buf);
-        //cin.getline(buf, 200);
-        //if (cordy != nullptr) {
-        //    cout << cordy << " is removed! \n";
-        //    delete[]cordy;
-        //}
-        //cordez = new char[strlen(buf) + 1];
-        //strcpy_s(cordez, strlen(buf) + 1, buf);
-        //cin.getline(buf, 200);
-        //if (cordez != nullptr) {
-        //    cout << cordez << " is removed! \n";
-        //    delete[]cordez;
-        //}
+        cordy = new char[strlen(buf) + 1];
+        strcpy_s(cordy, strlen(buf) + 1, buf);
+        cin.getline(buf, 200);
+        if (cordy != nullptr) {
+            cout << cordy << " is removed! \n";
+            delete[]cordy;
+        }
+        cordez = new char[strlen(buf) + 1];
+        strcpy_s(cordez, strlen(buf) + 1, buf);
+        cin.getline(buf, 200);
+        if (cordez != nullptr) {
+            cout << cordez << " is removed! \n";
+            delete[]cordez;
+        }
     }
     ~Cord() // деструктор
     {
@@ -81,22 +81,22 @@ public:
         return cordinatx;
     }
 
-    //void Setcordy(char* y)
-    //{
-    //    cordy = y;
-    //}
-    //char* Getcordy()
-    //{
-    //    return cordy;
-    //}
-    //void Setcordz(char* z)
-    //{
-    //    cordez = z;
-    //}
-    //char* Getcordz()
-    //{
-    //    return cordez;
-    //}
+    void Setcordy(char* y)
+    {
+        cordy = y;
+    }
+    char* Getcordy()
+    {
+        return cordy;
+    }
+    void Setcordz(char* z)
+    {
+        cordez = z;
+    }
+    char* Getcordz()
+    {
+        return cordez;
+    }
 };
 
 int main()
