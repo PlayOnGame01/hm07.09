8d78ceb5750ad7f332b01aa0b5a7e8e7894273fb
Artem <artemzhdanov1299@gmail.com>
2022-09-07 16:03:53 +0300

    Удаление точек 'y' и 'z'

--- hm07.09/Source.cpp
+++ hm07.09/Source.cpp
@@ -6,8 +6,7 @@ class Cord
 {
 private:
     char* cordinatx;
-    //char* cordy;
-    //char* cordez;
+
 
 public:
 
@@ -15,8 +14,7 @@ public:
     {
         cout << "Constructor by Cord\n";
         char* cordinatx = nullptr;
-        //char* cordy = nullptr;
-        //char* cordz = nullptr;
+
     }
 
     Cord(const char* x, const char* y, const char* z)
@@ -25,13 +23,7 @@ public:
         cordinatx = new char[strlen(x) + 1];
         strcpy_s(cordinatx, strlen(x) + 1, x);
 
-        ////cout << "Cord: y =\n";
-        //cordy = new char[strlen(y) + 1];
-        //strcpy_s(cordy, strlen(y) + 1, y);
 
-        ////cout << "Cord: z =\n";
-        //cordez = new char[strlen(z) + 1];
-        //strcpy_s(cordez, strlen(z) + 1, z);
 
     }
 
@@ -39,8 +31,7 @@ public:
     void Print()
     {
         cout << "Cord: x = " << cordinatx << "\n";
-        //cout << "Cord: y = " << cordy << "\n";
-        //cout << "Cord: z = " << cordez << "\n";
+
     }
     void Input() {
         char buf[200];
@@ -50,27 +41,13 @@ public:
             cout << cordinatx << " is removed! \n";
             delete[]cordinatx;
         }
-        //cordy = new char[strlen(buf) + 1];
-        //strcpy_s(cordy, strlen(buf) + 1, buf);
-        //cin.getline(buf, 200);
-        //if (cordy != nullptr) {
-        //    cout << cordy << " is removed! \n";
-        //    delete[]cordy;
-        //}
-        //cordez = new char[strlen(buf) + 1];
-        //strcpy_s(cordez, strlen(buf) + 1, buf);
-        //cin.getline(buf, 200);
-        //if (cordez != nullptr) {
-        //    cout << cordez << " is removed! \n";
-        //    delete[]cordez;
-        //}
+
     }
     ~Cord() // деструктор
     {
         cout << "Destructor\n";
         delete[]cordinatx;
-        //delete[]cordy;
-        //delete[]cordez;
+
     }
     void Setcordinatx(char* x)
     {
@@ -81,22 +58,6 @@ public:
         return cordinatx;
     }
 
-    //void Setcordy(char* y)
-    //{
-    //    cordy = y;
-    //}
-    //char* Getcordy()
-    //{
-    //    return cordy;
-    //}
-    //void Setcordz(char* z)
-    //{
-    //    cordez = z;
-    //}
-    //char* Getcordz()
-    //{
-    //    return cordez;
-    //}
 };
 
 int main()
